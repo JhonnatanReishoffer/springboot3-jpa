37f07d382df8822ddecb079686945d58340d57fb
Jhonnatan <jhonnatan.muniz@soulasalle.com>
2023-05-10 10:04:10 -0300

    Category

---
+++ src/main/java/com/course/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.course.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	
+}
